fd32a146352f8e6d704386d941f80420f1ab5e5f
dair68 <granthuang68@yahoo.com>
2022-01-16 23:58:07 -0800

    can read xl files

--- main.py
+++ main.py
@@ -7,5 +7,16 @@ Created on Sun Jan 16 15:36:16 2022
 
 import csv
 from logging import Logger
+from openpyxl import load_workbook
 
-print("Hello")
+#january filename: expedia_report_monthly_january_2018.xlsx
+f = input("Input filename: \n")
+print(f)
+
+wb = load_workbook(filename=f, read_only = True)
+ws = wb["Summary Rolling MoM"]
+print(ws)
+
+for row in ws.rows:
+    for cell in row:
+        print(cell.value)
