aea92105d615bc358db39dc04ed86f1160938d90
dair68 <granthuang68@yahoo.com>
2022-01-17 13:20:00 -0800

    month validation

--- main.py
+++ main.py
@@ -10,35 +10,39 @@ from logging import Logger
 from openpyxl import Workbook, load_workbook
 import sys
 
-
+months = ["january", "february", "march", "april", "may", "june", "july", 
+          "august", "september", "october", "november", "december"]
+month = ""
 wb = Workbook()
 
 #letting user type in file names until they successfully open xl file
 while True:
     try:
-        f = input("Input filename: \n")
-        #f = "expedia_report_monthly_january_2018.xlsx"
+        f = input("Input filename (.xlsx, .xlsm, .xltx, or .xltm file required): \n")
+        #f = "expedia_report_monthly_janusary_2018.xlsx"
         #f = "expedia_report_monthly_march_2018.xlsx"
         wb = load_workbook(filename=f, read_only = True)
-        break
     except FileNotFoundError:
-        print("Error: File does not exist")
+        print("Error: File not found")
     except Exception:
         print("Error: can't open file")
+    else:
+        #figuring out which month contained in filename
+        for m in months:
+            #found month contained within filename
+            if f.find(m.lower()) != -1:
+                month = m.lower()
+                break
+        
+        #found month in filename
+        if month != "":
+            break
+        else:
+            print("Can't find month in filename")
 
 ws = wb["Summary Rolling MoM"]
 print(ws)
         
-months = ["january", "february", "march", "april", "may", "june", "july", 
-          "august", "september", "october", "november", "december"]
-month = ""
-
-#figuring out which month contained in filename
-for m in months:
-    #found month contained within filename
-    if f.find(m) != -1:
-        month = m
-        break
     
 #found no month in filename
 if month == "":
